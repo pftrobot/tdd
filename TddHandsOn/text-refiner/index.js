bb0dcdf1edeb5321b62c5229a305a5d692c0174f
moldy <moldy@bookips.com>
2023-01-30 13:49:12 +0900

    unit test basic

--- TddHandsOn/text-refiner/index.js
+++ TddHandsOn/text-refiner/index.js
@@ -1,32 +1,5 @@
-function refineText(source, options) {
-  return [
-    normalizeWhitespaces,
-    compactWhitespaces,
-    maskBannedWords,
-    trimWhitespaces,
-  ].reduce((value, filter) => filter(value, options), source);
-}
-
-function normalizeWhitespaces(value) {
-  return value.replace("\t", " ");
-}
-
-function compactWhitespaces(value) {
-  return value.indexOf("  ") < 0
-    ? value
-    : compactWhitespaces(value.replace("  ", " "));
-}
-
-function maskBannedWords(value, options) {
-  return options ? options.bannedWords.reduce(maskBannedWord, value) : value;
-}
-
-function maskBannedWord(value, bannedWord) {
-  return value.replace(bannedWord, "*".repeat(bannedWord.length));
-}
-
-function trimWhitespaces(value) {
-  return value.trim();
+function refineText(s) {
+  return s.replace("    ", " ").replace("  ", " ")
 }
 
 module.exports = refineText;
--- TddHandsOn/text-refiner/index.test.js
+++ TddHandsOn/text-refiner/index.test.js
@@ -1,64 +1,43 @@
-const faker = require("faker");
-const sut = require("./index");
+const sut = require('./index')
 
+/*
+// 테스트 내용
+test('sut transforms "hello  world" to "hello world"', ()=>{
+    // 테스트 케이스
+    const actual = sut("hello  world")
+    // 테스트 케이스를 사용하여 실행했을 때 기대하는 결과값
+    expect(actual).toBe("hello world")
+})
+
+test('sut transforms "hello    world" to "hello world"', ()=>{
+    const actual = sut("hello    world")
+    expect(actual).toBe("hello world")
+})
+
+test('sut transforms "hello   world" to "hello world"', ()=>{
+    const actual = sut("hello   world")
+    expect(actual).toBe("hello world")
+})
+
+// 위 내용을 아래와 같이 간소화할 수 있다
+test('sut correctly works', ()=>{
+    for(const source of ['hello  world', 'hello   world', 'hello    world']){
+        const actual = sut(source)
+        expect(actual).toBe('hello world')
+    }
+})
+// 테스트 코드 작성 비용은 줄어들었지만, 어떤 케이스가 테스트에 실패했는지 알 수 없고 반복문 실행 중애 실패할 경우 그 뒤의 케이스들은 테스트를 실행하지 않는 문제가 있다.
+*/
+
+// jest parameterize 를 사용하여 위 문제 해결 가능
+// test data table 작성
 test.each`
-  source                 | expected
-  ${"hello  world"}      | ${"hello world"}
-  ${"hello   world"}     | ${"hello world"}
-  ${"hello    world"}    | ${"hello world"}
-  ${"hello     world"}   | ${"hello world"}
-  ${"hello      world"}  | ${"hello world"}
-  ${"hello       world"} | ${"hello world"}
-`('sut transforms "$source" to "$expected"', ({ source, expected }) => {
-  const actual = sut(source);
-  expect(actual).toBe(expected);
-});
+    source | expected
+    ${"hello  world"} | ${"hello world"}
+    ${"hello   world"} | ${"hello world"}
+    ${"hello    world"} | ${"hello world"}
+`('sut transforms "$source" to "$expected"', ({source, expected})=>{
+    const actual = sut(source)
+    expect(actual).toBe(expected)
 
-test.each`
-  source             | expected
-  ${"hello\t world"} | ${"hello world"}
-  ${"hello \tworld"} | ${"hello world"}
-`(
-  'sut transforms "$source" that contains \'\\t\' to "$expected"',
-  ({ source, expected }) => {
-    const actual = sut(source);
-    expect(actual).toBe(expected);
-  }
-);
-
-test.each`
-  source             | bannedWords              | expected
-  ${"hello mockist"} | ${["mockist", "purist"]} | ${"hello *******"}
-  ${"hello purist"}  | ${["mockist", "purist"]} | ${"hello ******"}
-`(
-  'sut transforms "$source" to "$expected"',
-  ({ source, bannedWords, expected }) => {
-    const options = { bannedWords };
-    const actual = sut(source, options);
-    expect(actual).toBe(expected);
-  }
-);
-
-describe("given banned word", () => {
-  const bannedWord = faker.lorem.word();
-  const masked = "*".repeat(bannedWord.length);
-  const source = `hello ${bannedWord}`;
-  const options = { bannedWords: [bannedWord] };
-
-  test(`"${bannedWord}" when invoke sut then it returns "hello ${masked}"`, () => {
-    const actual = sut(source, options);
-    expect(actual).toBe(`hello ${masked}`);
-  });
-});
-
-test.each`
-  source               | expected
-  ${" hello world"}    | ${"hello world"}
-  ${"  hello world"}   | ${"hello world"}
-  ${"hello world "}    | ${"hello world"}
-  ${"hello world  "}   | ${"hello world"}
-  ${"  hello world  "} | ${"hello world"}
-`('sut correctly trims "$source"', ({ source, expected }) => {
-  const actual = sut(source);
-  expect(actual).toBe(expected);
-});
+})
